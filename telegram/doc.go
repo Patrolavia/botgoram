de1f59923179eeae31e58cfd0a866c75b376065d
Ronmi Ren <ronmi.ren@gmail.com>
2016-06-02 17:36:45 +0800

    remove telegram api

--- telegram/doc.go
+++
@@ -1,8 +0,0 @@
-// This file is part of Botgoram
-// Botgoram is free software: see LICENSE.txt for more details.
-
-/*
-Package telegram represents Telegram Bot API in go way.
-This package is designed and rewrited from tucnak's telebot(https://github.com/tucnak/telebot).
-*/
-package telegram
